5e4c1dbc760e53c4e2c2a7d30ac0eec8ee0e8d0b
Oleg <olegyasinskiy95@gmail.com>
2025-06-09 21:25:21 +0300

    1

--- js/task-2.js
+++ js/task-2.js
@@ -9,10 +9,10 @@ function formatMessage(message, maxLength) {
   }
 }
 
-console.log(formatMessage("Curabitur ligula sapien", 14)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
-console.log(formatMessage("qweqweqweqwe", 55));
+console.log(formatMessage("qweqweqweqwe", 4));
